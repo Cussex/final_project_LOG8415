857ecb5c8a7d703e7fd5c8c01d85ce609a123a84
MaelCossec <mael.cossec@imt-atlantique.net>
2023-12-27 19:01:36 -0500

    Final results

--- client/client.py
+++ client/client.py
@@ -40,22 +40,29 @@ def main():
     args = parser.parse_args()
     gatekeeper_dns = args.gatekeeper_dns    
 
-    select_actor_query = "SELECT * FROM actor WHERE first_name = 'Scarlett' and last_name = 'Johansson';"
-    insert_actor_query = "INSERT INTO actor (first_name, last_name) VALUES ('John', 'Doe');"
-    delete_actor_query = "DELETE FROM actor WHERE first_name = 'Scarlett' and last_name = 'Johansson';"
+    select_actor_query = "SELECT * FROM actor WHERE first_name = 'JENNIFER' and last_name = 'DAVIS';"
+    select_actor_query2 = "SELECT * FROM actor WHERE first_name = 'JOHN' and last_name = 'DOE';"
+    insert_actor_query = "INSERT INTO actor (first_name, last_name) VALUES ('JOHN', 'DOE');"
+    delete_actor_query = "DELETE FROM actor WHERE first_name = 'JENNIFER' and last_name = 'DAVIS';"
 
     # Should work
-    print("\nSelecting from actor table using direct query")
+    print("\nSelecting from actor table using direct query (Should work : selecting JENNIFER DAVIS)")
     print(select_direct(select_actor_query, gatekeeper_dns))
+    # Should work but empty response
+    print("\nSelecting from actor table using direct query (Should work but empty response : selecting JOHN DOE)")
+    print(select_direct(select_actor_query2, gatekeeper_dns))
     # Should work
-    print("\nInserting into actor table using direct query")
+    print("\nInserting into actor table using direct query (Should work : inserting JOHN DOE)")
     print(insert_direct(insert_actor_query, gatekeeper_dns))
     # Should work
-    print("\nSelecting from actor table using random query")
+    print("\nSelecting from actor table using random query (Should work : selecting JENNIFER DAVIS)")
     print(select_random(select_actor_query, gatekeeper_dns))
     # Should work
-    print("\nSelecting from actor table using custom query")
+    print("\nSelecting from actor table using custom query (Should work : selecting JENNIFER DAVIS)")
     print(select_custom(select_actor_query, gatekeeper_dns))
+    # Should work
+    print("\nSelecting from actor table using custom query (Should work : selecting JOHN DOE)")
+    print(select_custom(select_actor_query2, gatekeeper_dns))
     # Shouldn't work
     print("\nDeleting from actor table using direct query")
     print(insert_direct(delete_actor_query, gatekeeper_dns))
